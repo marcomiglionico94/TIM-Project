d01101a2118ab4a8e99448c79ec78db783c1dfa1
ramiropoli <ramiropolidelanghe@gmail.com>
2016-07-13 20:08:02 -0300

    Add files via upload

---
+++ hypermedia2.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jul 14, 2016 at 12:50 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 5.6.23
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hypermedia2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `actrules`
+--
+-- in use(#1932 - Table 'hypermedia2.actrules' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'hypermedia2.actrules' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `description`
+--
+-- in use(#1932 - Table 'hypermedia2.description' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'hypermedia2.description' doesn't exist in engine)
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faq`
+--
+-- in use(#1932 - Table 'hypermedia2.faq' doesn't exist in engine)
+-- Error reading data: (#1932 - Table 'hypermedia2.faq' doesn't exist in engine)
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
